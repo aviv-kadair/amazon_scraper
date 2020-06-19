5c5eea5aec8f88f607be3ceab190d8b526be792e
Aviv Kadair <avivkadair@gmail.com>
2020-06-19 15:41:54 +0300

    Fixed selenium timeout and language problem, tested on Aviv's laptop

--- get_profile.py
+++ get_profile.py
@@ -9,6 +9,8 @@ def user_profile(my_url):
     cwd = os.getcwd()
     options = se.webdriver.ChromeOptions()
     options.add_argument('headless chrome=83.0.4103.106')
+    options.add_argument("enable-features=NetworkServiceInProcess")
+    options.add_argument("--lang=en-US")
     driver = se.webdriver.Chrome(cwd + "\\chromedriver.exe", options=options)
     driver.get("https://www.amazon.com" + my_url)
     p_element = driver.find_element_by_id(id_='profile_v5')
@@ -38,5 +40,5 @@ def user_profile(my_url):
     return reviewer_ranking,reviews,votes
 
 
-my_url = '//gp/profile//amzn1.account.AEMRAZPNN2NBBUDV4YGGYMGPFC6A//ref=cm_cr_dp_d_gw_tr?ie=UTF8'
+my_url = '/gp/profile/amzn1.account.AGZHZLWZGTY5L5FESNQ5VCKP24UQ/ref=cm_cr_arp_d_gw_btm?ie=UTF8'
 print(user_profile(my_url))
