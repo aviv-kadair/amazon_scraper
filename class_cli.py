49873de7cc4f44f57dc5931bc3dfa3f96b18b1f7
Aviv Kadair <avivkadair@gmail.com>
2020-07-04 20:47:18 +0300

    cli class

--- class_cli.py
+++ class_cli.py
@@ -17,7 +17,18 @@ class cli_tool(cmd.Cmd):
 
     def set_search_parameter(self, ):
         parser_param = argparse.ArgumentParser
-        parser_param.add_option(dest = 'ranking', type=str, help='minimal item ranking')
-        parser_param.add_option(dest = 'manufacture', type=str, help='desired manufacture')
-        parser_param.add_option(dest='screen_size', type=str, help='desired screen size')
-        parser_param.add_option(dest='RAM_size', type=str, help='desired RAM size')
+        parser_param.add_option(dest = 'ranking', type=int, help='minimal item ranking', choices=(1, 2, 3, 4, 5))
+        parser_param.add_option(dest = 'manufacture', type=str, help='desired manufacture', choices=('Acer', 'Dell',
+                                                                                                     'HP', 'Samsung',
+                                                                                                     'Lenovo','Asus',
+                                                                                                     'Apple'))
+        parser_param.add_option(dest='screen_size', type=str, help='desired screen size', choices=('11-', '11-12',
+                                                                                                   '12-13', '13-14',
+                                                                                                   '14-15', '15-16',
+                                                                                                   '16-17', '17+'))
+        parser_param.add_option(dest='RAM_size', type=str, help='desired RAM size', choices=('4', '8', '12', '16',
+                                                                                             '32', '64'))
+        parser_param.add_option(dest='weight', type=str, help='desired laptop weight in pounds',
+                                choices=('3-', '3-4', '4-5', '5-6', '7-8', '8+'))
+        parser_param.add_option(dest='HD_type', type=str, help='desired hard drive type',
+                                choices=('SSD', 'HDD', 'Hybrid'))
