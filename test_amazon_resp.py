5fe460d56975b31c8bba575fc3cd799c6d1ae281
seraht <serah1995@hotmail.fr>
2020-07-29 00:05:56 +0300

    keep directories

--- test_amazon_resp.py
+++ test_amazon_resp.py
@@ -9,12 +9,11 @@ from Logging import logger
 from time import sleep
 from random import randint
 
-from Scraping.scraper_class import Scraper
 
 proxies_list = config.PROXIES_LIST
 headers = config.HEADERS
-proxy = {'http': random.choice(Scraper.proxies_list)}
+
 url = 'https://www.amazon.com/s?k=laptop&qid=1591707465&ref=sr_pg_2'
-web_page = requests.get(url, headers=Scraper.headers, proxies=proxy)
+web_page = requests.get(url, headers=headers)
 content = web_page.content
 print(content)
